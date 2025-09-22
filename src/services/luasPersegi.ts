fd7877169bec685e0c96b1225bd271ba51fbec1e
RamaRaditya-s <misterfalcon.5512@gmail.com>
2025-09-22 13:08:39 +0700

    Update belah ketupat program

--- src/services/luasPersegi.ts
+++ src/services/luasPersegi.ts
@@ -1,7 +1,9 @@
-export function luasPersegi(sisi: number): void {
+export function luasPersegi(sisiInput: string): void {
   console.log("\n##  Program TypeScript Luas Persegi ##");
   console.log("=========================================\n");
 
+  const sisi = parseFloat(sisiInput); // ubah string ke float
   const luas = sisi * sisi;
-    console.log(`Luas Persegi: ${luas}`);
+
+  console.log(`Luas Persegi: ${luas}`);
 }
